9f0b639bf587796169ea6488f99328db2d19d31a
Jeeva <jeevai1806@gmail.com>
2026-09-14 07:35:45 +0000

    Corrected intentation and comments to the program

--- decode.c
+++ decode.c
@@ -12,20 +12,21 @@ Status read_and_validate_decode_args(int argc, char *argv[], DecodeInfo *decInfo
     /* Validate argument count */
     if (argc < 3 || argc > 4)
     {
-        printf("Invalid number of arguments\n");
-        return e_failure;
+        printf("Invalid number of arguments\n"); //Printing the error message
+        return e_failure; //returns error to the main
     }
 
     /* Validate the .bmp input image */
     if (argv[2] != NULL && strstr(argv[2], ".") != NULL &&
         strcmp(strstr(argv[2], "."), ".bmp") == 0)
     {
-        decInfo->stego_image_fname = argv[2];
+        //Fetch and store the image file(.bmp) in the stucture member (stego_image_fname)
+        decInfo -> stego_image_fname = argv[2];
     }
     else
     {
-        printf("Enter a valid input .bmp file\n");
-        return e_failure;
+        printf("Enter a valid input .bmp file\n"); //Displaying error message 
+        return e_failure; //return error
     }
 
     /* Check whether user provided output filename */
@@ -34,12 +35,13 @@ Status read_and_validate_decode_args(int argc, char *argv[], DecodeInfo *decInfo
         if (argv[3] != NULL && strstr(argv[3], ".") != NULL &&
             strcmp(strstr(argv[3], "."), ".txt") == 0)
         {
+            //Fetch and store the output text file into the stucture member (out_secret_txt)
             decInfo->out_secret_txt = argv[3];
         }
         else
         {
-            printf("Enter a valid output text file\n");
-            return e_failure;
+            printf("Enter a valid output text file\n"); //Displays the error message
+            return e_failure; //returing error
         }
     }
     else
@@ -48,5 +50,5 @@ Status read_and_validate_decode_args(int argc, char *argv[], DecodeInfo *decInfo
         decInfo->out_secret_txt = "out_sec.txt";
     }
 
-    return e_success;
+    return e_success; //After all the process returns success to the main 
 }
